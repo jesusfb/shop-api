c487e0ad0b85a526a4a0b6d914aadf4a9f88988f
51fe <riafan@hotmail.com>
2023-06-18 01:53:17 +0800

    add sort by _id

--- controllers/manufacturer.js
+++ controllers/manufacturer.js
@@ -1,41 +1,46 @@
 const Model = require('../model');
-const {Manufacturer} = Model;
+const { Product, Manufacturer } = Model;
 
 const manufacturerController = {
   all(req, res) {
-    Manufacturer.find({})
-      .exec((err, manufacturers) => res.json(manufacturers)
-      )
+    Manufacturer.find()
+      .sort({ _id: -1 })
+      .exec((err, manufacturers) => res.json(manufacturers));
   },
 
   byId(req, res) {
     const idParam = req.params.id;
-    Manufacturer
-      .findOne({_id: idParam})
-      .exec((err, manufacturer) => res.json(manufacturer));
+    Manufacturer.findOne({ _id: idParam }).exec((err, manufacturer) =>
+      res.json(manufacturer)
+    );
   },
   create(req, res) {
     const requestBody = req.body;
     const newManufacturer = new Manufacturer(requestBody);
     newManufacturer.save((err, saved) => {
       if (saved) {
-        Manufacturer
-          .findOne({_id: saved._id})
-          .exec((err, manufacturer) => res.json(manufacturer));
+        Manufacturer.findOne({ _id: saved._id }).exec((err, manufacturer) =>
+          res.json(manufacturer)
+        );
       }
-    })
+    });
   },
   update(req, res) {
     const idParam = req.params.id;
     let manufacturer = req.body;
-    Manufacturer.findOne({_id: idParam}, (err, data) => {
+    Manufacturer.findOne({ _id: idParam }, (err, data) => {
       data.name = manufacturer.name;
       data.save((err, updated) => res.json(updated));
-    })
+    });
   },
   remove(req, res) {
     const idParam = req.params.id;
-    Manufacturer.findOne({_id: idParam}).remove((err, removed) => res.json(idParam))
+    Manufacturer.findOne({ _id: idParam }).remove((err, removed) =>
+      Product.find({
+        // remove products too
+        manufacturer: idParam
+      }).remove(() => res.json(idParam))
+    );
   }
 };
 
--- controllers/product.js
+++ controllers/product.js
@@ -1,21 +1,21 @@
 const Model = require('../model');
-const {Product} = Model;
+const { Product } = Model;
 
 const productController = {
   all(req, res) {
     // Returns all products
-    Product.find({})
-    // alongside it's manufacturer
-    // information
+    Product.find()
+      .sort({ _id: -1 })
+      // alongside it's manufacturer
+      // information
       .populate('manufacturer')
-      .exec((err, products) => res.json(products))
+      .exec((err, products) => res.json(products));
   },
   byId(req, res) {
     const idParam = req.params.id;
     // Returns a single product
     // based on the passed in ID parameter
-    Product
-      .findOne({_id: idParam})
+    Product.findOne({ _id: idParam })
       // as well as it's manufacturer
       .populate('manufacturer')
       .exec((err, product) => res.json(product));
@@ -29,32 +29,29 @@ const productController = {
     newProduct.save((err, saved) => {
       // Returns the saved product
       // after a successful save
-      Product
-        .findOne({_id: saved._id})
+      Product.findOne({ _id: saved._id })
         .populate('manufacturer')
         .exec((err, product) => res.json(product));
-    })
+    });
   },
   update(req, res) {
     const idParam = req.params.id;
     let product = req.body;
     // Finds a product to be updated
-    Product.findOne({_id: idParam}, (err, data) => {
-      // Updates the product payload
-      data.name = product.name;
-      data.description = product.description;
-      data.image = product.image;
-      data.price = product.price;
-      data.inventory = product.inventory;
-      data.manufacturer = product.manufacturer;
-      // Saves the product
-      data.save((err, updated) => res.json(updated));
-    })
+    Product.findOneAndUpdate({ _id: idParam }, product, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.json(result);
+    });
   },
   remove(req, res) {
     const idParam = req.params.id;
     // Removes a product
-    Product.findOne({_id: idParam}).remove((err, removed) => res.json(idParam))
+    Product.findOne({ _id: idParam }).remove((err, removed) =>
+      res.json(idParam)
+    );
   }
 };
 
